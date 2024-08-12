74af0ed45ef8e21b423fbc2f781a3d50e0882a0c
dlvmfquf <thsgustmd5@naver.com>
2024-08-12 21:49:23 +0900

    손현승 Day2

--- Day1/Day2/Day2/main08.cpp
+++ Day1/Day2/Day2/main08.cpp
@@ -8,7 +8,7 @@ void main()
 	printf("숫자(정수)를 입력해주세요: ");
 	scanf_s("%d", &number);
 	if (number % 2 == 0)
-		printf("%d는 짝수입니다.\n", number);
+		printf("%d은(는) 짝수입니다.\n", number);
 	else
-		printf("%d는 홀수입니다.\n", number);
+		printf("%d은(는) 홀수입니다.\n", number);
 }
